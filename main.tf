8f4157b8df7c321e76e3b86f048e5bd75d11fc26
GiovanniN46 <gianni.olino97@gmail.com>
2020-04-30 11:00:15 +0200

    Aggiunto ALoadBalancer e il meccanismo DNS

--- main.tf
+++ main.tf
@@ -11,6 +11,11 @@ data "aws_subnet_ids" "default"{
 
 }
 
+variable "server_port"{
+  type = number
+  default = 8080
+}
+
 resource "aws_launch_configuration" "example" {
   image_id           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
@@ -33,6 +38,9 @@ resource "aws_autoscaling_group" "example"{
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier = data.aws_subnet_ids.default.ids
 
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
   min_size=2
   max_size=10
 
@@ -49,9 +57,101 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+
+resource "aws_lb" "example"{
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default.ids
+  security_groups = [aws_security_group.alb.id]
+
+}
+
+resource "aws_lb_listener" "http"{
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  #di default ritorno a 404 propagate_at_launch
+  default_action{
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page non found"
+      status_code = 404
+    }
+
+  }
+
+}
+
+
+resource "aws_security_group" "alb"{
+  name = "terraform-example-alb"
+
+  #Allow inboud HTTP request
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Allow all outbound request
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_lb_target_group" "asg"{
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check{
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+
+  }
+
+}
+
+
+resource "aws_lb_listener_rule" "asg"{
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition{
+    field = "path-pattern"
+    values = ["*"]
+  }
+
+  action{
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+
+}
+
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "Il Domain Name del load balancer"
 }
